2b08a5b2538bb89e0541f9fdd405b03a6f9da57f
JCmixia25 <jcastillom31@miumg.edu.gt>
2024-05-26 16:47:33 -0600

    JCmixia
    Validacion de usuario

--- src/main/java/Controller/inicioController.java
+++ src/main/java/Controller/inicioController.java
@@ -15,6 +15,66 @@ import javax.faces.bean.ManagedBean;
 @ManagedBean(name = "bkn_inicio")
 public class inicioController implements Serializable  {
 
+    
+    private String usuario;
+    private String contraseña;
+    private int rptLogin;
+    private List<Usuario> listaUsuarios;
+
+    @PostConstruct
+    public void init() {
+        // Puedes inicializar algún dato aquí si es necesario
+        System.out.println("");
+        try{
+            validarUsuario();
+        }catch(Exception e){
+            System.out.println("Error en el inicio");
+        }
+        
+    }
+    
+    public void validarUsuario() throws Exception{
+        
+        ConsultasDAO consulta = new ConsultasDAO();
+        try{
+          
+        } catch(Exception e){
+            
+        }
+        
+        System.out.println("Respuesta es: "+rptLogin);
+        
+    }
+
+    
+    /**
+     * @return the usuario
+     */
+    public String getUsuario() {
+        return usuario;
+    }
+
+    /**
+     * @param usuario the usuario to set
+     */
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    /**
+     * @return the contraseña
+     */
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    /**
+     * @param contraseña the contraseña to set
+     */
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+    
     /**
      * @return the listaUsuarios
      */
@@ -29,13 +89,4 @@ public class inicioController implements Serializable  {
         this.listaUsuarios = listaUsuarios;
     }
     
-    private List<Usuario> listaUsuarios;
-
-    @PostConstruct
-    public void init() {
-        // Puedes inicializar algún dato aquí si es necesario
-        
-    }
-    
-
 }
