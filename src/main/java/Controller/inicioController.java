68004bb9f98bcaf0334ca77ef1d92fc10320a5c6
EduardoJerez123 <146893598+EduardoJerez123@users.noreply.github.com>
2024-05-26 15:34:27 -0600

    inicioController

--- src/main/java/Controller/inicioController.java
+++ src/main/java/Controller/inicioController.java
@@ -4,21 +4,49 @@
  */
 package Controller;
 
+import DAO.ConsultasDAO;
 import java.io.Serializable;
+import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 
-/**
- *
- * @author CompuFire
- */
+
 @ManagedBean(name = "bkn_inicio")
 public class inicioController implements Serializable  {
     
-    
+    private List<inicioController> listaUsuarios;
+
     @PostConstruct
     public void init() {
         // Puedes inicializar algún dato aquí si es necesario
-        
+        listarUsuarios();
     }
+    
+        public void listarUsuarios() {
+
+        ConsultasDAO consulta = new ConsultasDAO();
+
+        try {
+            setListaUsuarios(consulta.consultarUsuarios());
+            System.out.println("Usuarios: " + consulta.consultarUsuarios());
+        } catch (Exception e) {
+            System.out.println("Error al listar usuarios");
+        }
+
+    }
+    
+        /**
+     * @return the listaUsuarios
+     */
+    public List<inicioController> getListaUsuarios() {
+        return listaUsuarios;
+    }
+
+    /**
+     * @param listaUsuarios the listaUsuarios to set
+     */
+    public void setListaUsuarios(List<inicioController> listaUsuarios) {
+        this.listaUsuarios = listaUsuarios;
+    }
+
 }
