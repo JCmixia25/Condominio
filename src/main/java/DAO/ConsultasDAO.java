18048b07c541b3696fbfd23ce43ed12ca6c5088d
angel7ramos <128450159+angel7ramos@users.noreply.github.com>
2024-05-29 00:43:55 -0600

    angel
    manto

--- src/main/java/DAO/ConsultasDAO.java
+++ src/main/java/DAO/ConsultasDAO.java
@@ -5,6 +5,7 @@
 package DAO;
 
 import Models.Usuario;
+import Models.Reporte;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
@@ -51,5 +52,51 @@ public class ConsultasDAO {
 
         return Usuarios;
     }
+    
+    //listar reportes
+     public List<Reporte> consultarReporte() throws Exception {
+        List<Reporte> Reportes = new ArrayList<Reporte>();
+
+        try {
+            String query = "SELECT id_reporte, autor_id, usuario_asignado_id, descripcion, imagen, fecha_creacion, fecha_cierre, estado FROM reporte";
+            Statement s = con.conexionMysql().createStatement();
+            ResultSet r = s.executeQuery(query);
+                  
+
+            while (r.next()) {
+                Reporte datoss = new Reporte();
+                
+                datoss.setId_reporte(r.getLong("id_reporte"));
+                datoss.setAutor_id(r.getLong("autor_id"));
+                datoss.setUsuario_asignado_id(r.getLong("usuario_asignado_id"));
+                datoss.setDescripcion(r.getString("descipcion"));
+                datoss.setImagen(r.getLong("imagen"));
+                datoss.setFecha_creacion(r.getString("fecha_creacion"));
+                datoss.setFecha_cierre(r.getString("fecha_cierre"));
+                datoss.setEstado_reporte(r.getString("estado"));
+                Reportes.add(datoss);
+                
+            
+            }
+            
+ 
+        } catch (Exception e) {
+            System.out.println("Error al consultar reporte");
+        } finally {
+            if (con != null) {
+                try {
+                    con.conexionMysql().close();
+                    System.out.println("Cierre de conexion exitosa");
+                } catch (SQLException ex) {
+                    System.out.println("Error al cerrar conexion");
+                }
+            }
+        }
+
+        return Reportes;
+     
+     }
+   
+
 
 }
